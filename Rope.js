fb60276324ebe0ab63080e3749d9b8fc37c9b6ba
neekith <84722963+neekith@users.noreply.github.com>
2022-01-18 16:45:41 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -20,7 +20,7 @@ class Rope {
 
       //set the value of stroke to hide the rope
       stroke(48, 22, 8);
-      strokeWeight(0);
+      //strokeWeight();
 
       line(pointB.x, pointB.y, pointA.x, pointA.y);
 
